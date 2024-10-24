766d823054becd8db5b6f24b9d4fcbf0d24fd696
Maxxxxxx-x <maxxng1028@gmail.com>
2024-10-24 23:21:58 +0800

    feat: updated schemas to use ULID

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,73 +1,69 @@
-CREATE TYPE Roles AS ENUM ("owner", "superadmin", "admin");
-CREATE TYPE UserActions AS ENUM ("create-account", "get-accounts", "login-account", "update-account", "delete-account", "send-creation-email");
-CREATE TYPE TokenActions AS ENUM("create-token", "revoke-token")
+CREATE TYPE Roles AS ENUM ("owner", "admin", "user");
+CREATE TYPE UserActions AS ENUM ("create-account", "get-accounts", "login-account", "update-account", "delete-account", "send-invite-mail");
+CREATE TYPE TokenActions AS ENUM ("create-token", "get-tokens", "revoke-token");
 
-CREATE TABLE IF NOT EXISTS Users(
-    Id CHAR(26) PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS Users (
+    Id UUID PRIMARY KEY,
     Email TEXT UNIQUE NOT NULL,
     Password TEXT NOT NULL,
     Role Roles NOT NULL,
-    CreatedAt BIGINT,
-    UpdatedAt BIGINT
+    CreatedAt TIMESTAMPTZ NOT NULL,
+    UpdatedAt TIMESTAMPTZ
 );
 
-CREATE TABLE IF NOT EXISTS KeyPairs(
-    Id CHAR(26) PRIMARY KEY,
-    PublicKey BYTEA NOT NULL,
-    CreatedAt BIGINT
+
+CREATE TABLE IF NOT EXISTS KeyPairs (
+    Id UUID PRIMARY KEY,
+    PublicKey BYTEA UNIQUE NOT NULL,
+    CreatedAt TIMESTAMPTZ
 );
 
-CREATE TABLE IF NOT EXISTS Tokens(
-    Id CHAR(26) PRIMARY KEY,
-    CreatorId CHAR(26) NOT NULL,
-    KeyId CHAR(26) NOT NULL,
-    Name CHAR(20) NOT NULL,
-    ExpiresAt BIGINT,
-    IsRevoked BOOLEAN DEFAULT FALSE,
-    CreatedAt BIGINT,
-    FOREIGN KEY (KeyId) REFERENCES KeyPairs(Id),
+
+CREATE TABLE IF NOT EXISTS Tokens (
+    Id UUID PRIMARY KEY,
+    CreatorId UUID NOT NULL,
+    KeyId UUID NOT NULL,
+    TokenName CHAR(20) NOT NULL,
+    ExpiresAt TIMESTAMPTZ,
+    IsRevoked BOOLEAN NOT NULL DEFAULT FALSE,
+    RevokedBy UUID NOT NULL,
+    CreatedAt TIMESTAMPTZ NOT NULL,
     FOREIGN KEY (CreatorId) REFERENCES Users(Id),
-    FOREIGN KEY (RevokedBy) REFERENCES Users(Id) ON DELETE RESTRICT
+    FOREIGN KEY (KeyId) REFERENCES KeyPairs(Id),
+    FOREIGN KEY (RevokedBy) REFERENCES Users(Id)
 );
 
-CREATE TABLE IF NOT EXISTS UsageLogs(
-    Id CHAR(26)  PRIMARY KEY,
-    TokenId CHAR(26) NOT NULL,
+
+CREATE TABLE IF NOT EXISTS UsageLogs (
+    Id UUID PRIMARY KEY,
+    TokenId UUID NOT NULL,
     CallerIP CIDR NOT NULL,
     Subject TEXT NOT NULL,
     From TEXT NOT NULL,
     To TEXT NOT NULL,
-    FilePath TEXT NOT NULL,
-    CreatedAt BIGINT,
+    FilePath TEXT,
+    CreatedAt TIMESTAMPTZ NOT NULL,
     FOREIGN KEY (TokenId) REFERENCES Tokens(Id)
 );
 
-CREATE TABLE IF NOT EXISTS UserLogs(
-    Id CHAR(26) PRIMARY KEY,
-    UserId CHAR(26) NOT NULL,
+CREATE TABLE IF NOT EXISTS UserLogs (
+    Id UUID PRIMARY KEY,
+    UserId UUID NOT NULL,
     ActionType UserActions NOT NULL,
     IPAddress CIDR NOT NULL,
     UserAgent TEXT NOT NULL,
-    CreatedAt BIGINT,
+    CreatedAt TIMESTAMPTZ NOT NULL,
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
-CREATE TABLE IF NOT EXISTS TokenLogs(
-    Id CHAR(26) PRIMARY KEY,
-    UserId CHAR(26) NOT NULL,
-    TokenId CHAR(26) NOT NULL,
+CREATE TABLE IF NOT EXISTS TokenLogs (
+    Id UUID PRIMARY KEY,
+    UserId UUID NOT NULL,
+    TokenId UUID NOT NULL,
     ActionType TokenActions NOT NULL,
     IPAddress CIDR NOT NULL,
-    CreatedAt BIGINT,
+    CreatedAt TIMESTAMPTZ NOT NULL,
     FOREIGN KEY (UserId) REFERENCES Users(Id),
     FOREIGN KEY (TokenId) REFERENCES Tokens(Id)
 );
-
-CREATE INDEX IF NOT EXISTS UsersIdx ON Users (Email);
-CREATE INDEX IF NOT EXISTS TokenIdx ON Tokens(CreatorId);
-CREATE INDEX IF NOT EXISTS UsageLogs_TokenIdx ON UsageLogs(TokenId);
-CREATE INDEX IF NOT EXISTS UsageLogs_IpIdx ON UsageLogs(CallerIP);
-CREATE INDEX IF NOT EXISTS UserLogs_UserIdx ON UserLogs(UserId);
-CREATE INDEX IF NOT EXISTS UserLogs_ActionIdx ON UserLogs(ActionType);
-CREATE INDEX IF NOT EXISTS TokenLogs_UserIdx ON TokenLogs(UserId);
-CREATE INDEX IF NOT EXISTS TokenLogs_TokenIdx ON TokenLogs(ActionType);
